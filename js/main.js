4eeb0505355de2279714516e448ba7eec94973b2
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-11-17 18:27:32 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+let slides = document.querySelectorAll(".slide");
+let i = 0;
 
+display(i);
+
+function display(i) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[i].style.display = 'flex';
+}
+
+function nextSlide() {
+    i++;
+    if (i > slides.length - 1) {
+        i = 0;
+    }
+    display(i);
+}
+
+function prevSlide() {
+    i--;
+    if (i < 0) {
+        i = slides.length - 1;
+    }
+    display(i);
+}
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
